868262cce43906a7406a0fe120bb59a289658ed3
CHELSI SINGH <chelsiji345@gmail.com>
2025-04-24 23:39:12 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter how many Fibonacci numbers to print: ");
+        int n = scanner.nextInt();
+
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
