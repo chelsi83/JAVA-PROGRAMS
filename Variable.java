c702f6b47b56699ec24d270918c8b729d929bd95
CHELSI SINGH <chelsiji345@gmail.com>
2025-04-30 22:39:16 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Variable {
+    public static void main(String[] args) {
+
+        int num;
+        num = 100;
+        System.out.println("This is num: " + num);
+        num = num * 2;
+        System.out.print("The value of num * 2 is ");
+        System.out.println(num);
+    }
+}
